f59a0c7f8fcabb0d96ec4ec164d06e4cce45b0fd
DerToaster1138, Jakob Drexler <Jakob.Drexler@sycor.de>
2023-12-08 10:27:02 +0000

    Console Running, cases and escape case added

---
+++ EVEMarketeer Console/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EVEMarketeer_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Creds();
+            Spacer(2);
+            StartInfo();
+            ParseCommand();
+
+        }
+
+        public static void Creds()
+        {
+            Console.Title = "EVE ONLINE CONSOLE V0.1";
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine("EVEOnline Helper");
+            Console.WriteLine("By DerToaster1138/Tanam Rotsuda");
+            Console.WriteLine("DO NOT REDISTRIBUTE");
+            Console.WriteLine("Work in progress");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        public static void StartInfo()
+        {
+            Console.WriteLine("Available Commands:");
+            Console.WriteLine("   [M]arketinformations");
+            Console.WriteLine("   [T]ypeinformation");
+            Console.WriteLine("   [C]lear Screen");
+            Spacer(2);
+        }
+
+        public static void Spacer(int space)
+        {
+            for (int i = 0; i < space; i++)
+            {
+                Console.WriteLine();
+            }
+        }
+        public static void ClearScreen()
+        {
+            Console.Clear();
+        }
+
+        public static void ParseCommand()
+        {
+            ConsoleKeyInfo pressed = Console.ReadKey();
+            switch(pressed.Key)
+            {
+
+                case ConsoleKey.C:
+                    ClearScreen();
+                    break;
+
+                case ConsoleKey.M:
+                    //implement marketChecker
+                    break;
+
+                case ConsoleKey.T:
+                    //implement Type Checker
+                    break;
+
+                case ConsoleKey.F9:
+                    Console.Beep(2000, 5);
+                    break;
+            }
+            if (pressed.Key != ConsoleKey.Escape)
+            {
+                ParseCommand();
+            }
+            else 
+            {
+                Console.WriteLine("Exiting");
+            }
+
+        }
+    }
+}
