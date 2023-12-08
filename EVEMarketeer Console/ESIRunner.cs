e0756224dbcfbef52dcbef7371be1908f4c32a24
DerToaster1138, Jakob Drexler <Jakob.Drexler@sycor.de>
2023-12-08 11:47:59 +0000

    added ESI Class

---
+++ EVEMarketeer Console/ESIRunner.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Net;
+using System.IO.Compression;
+
+namespace EVEMarketeer_Console
+{
+    class ESIRunner
+    {
+        public void Run(string url) 
+        {
+            System.Net.WebClient client = new System.Net.WebClient();
+            string data = client.DownloadString(url);
+        }
+
+        public void UrlConstructor() 
+        {
+            string url = "https://esi.evetech.net/latest/";
+            url += "";
+        }
+    }
+}
